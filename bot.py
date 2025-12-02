ef0a0c31783a3ec900ac1894a799a6ad1b0fdd88
bunny037373 <scottymcd1234@gmail.com>
2025-12-01 20:19:41 -0600

    Fix: add intents to Bot initialization

--- bot.py
+++ bot.py
@@ -4,16 +4,16 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Set intents properly
 intents = discord.Intents.default()
 intents.message_content = False
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f'Logged in as {bot.user}')
 
 bot.run(TOKEN)
