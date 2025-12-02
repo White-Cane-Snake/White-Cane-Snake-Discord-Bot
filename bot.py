6363faed37240fdd50e137586ab2abfa32272bc0
bunny037373 <132156542+bunny037373@users.noreply.github.com>
2025-12-01 20:09:44 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="!")
+# Use default intents WITHOUT privileged message_content
+intents = discord.Intents.default()
+intents.message_content = False  # Disable privileged intent
+
+bot = commands.Bot(command_prefix="!", intents=intents)  # <-- make sure intents are passed here
 
 @bot.event
 async def on_ready():
